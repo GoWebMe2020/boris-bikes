958f27e86f6e6b432d86fe358835bc275cd8dc23
JP Ferreira <jpferreira@MA301.broadband>
2021-04-28 11:52:34 +0100

    Testing method release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
-require 'docking_station'
+require './lib/docking_station'
 
 describe DockingStation do
-    
+    describe "#release_bike" do
+        it { is_expected.to respond_to(:release_bike) }
+    end
 end
