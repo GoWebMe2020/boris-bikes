7b2a4077c36d6db89b52925b4df0bb972408dad8
JP Ferreira <jpferreira@MA301.broadband>
2021-04-28 16:03:56 +0100

    raise error when no bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,14 @@
 
 class DockingStation
     attr_reader :bike
-    #def initialize (bikes)
-    #    @bikes = bikes    
-    # end
-    def release_bike
-        Bike.new
-    end
     def docking_bike(bike)
         @bike = bike
     end
     def bike
+        @bike
+    end
+    def release_bike
+        fail 'No bike' unless @bike
         @bike
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,14 +2,22 @@ require './lib/docking_station'
 
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
-    it 'releases a working bike' do
-        bike = subject.release_bike
-        expect(bike).to be_working
+    describe '#release_bike' do
+        it 'releases a working bike' do
+            bike = Bike.new
+            subject.docking_bike(bike)
+            expect(subject.release_bike).to eq bike
+        end
     end
     it { is_expected.to respond_to(:docking_bike).with(1).argument }
     it 'Docking a bike' do
         bike = Bike.new
         expect(subject.docking_bike(bike)).to eq bike
+    end
+    describe '#release_bike' do
+        it 'raises an error if no bike' do
+            expect { subject.release_bike }.to raise_error 'No bike'
+        end
     end
 end
 
