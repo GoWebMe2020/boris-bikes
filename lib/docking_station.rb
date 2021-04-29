22f88f5d6929ff6a61168c5ed9764363d0d9f9f4
JP Ferreira <jpferreira@MA301.broadband>
2021-04-29 16:56:21 +0100

    adding up to step 17

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,32 @@
-require 'bike'
+require_relative 'bike'
 
 class DockingStation
-    attr_reader :bike
-    def bike
-        @bike
-    end
-    def docking_bike(bike)
-        fail 'No dock' if @bike
-        @bike = bike
-    end
-    def release_bike
-        fail 'No bike' unless @bike
-        @bike
-    end
+  attr_reader :bike
+  attr_reader :capacity
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @bike = []
+    @capacity = capacity
+  end
+
+  def release_bike
+    fail "No bike available" if dock_empty?
+    @bike
+  end
+
+  def dock(bike)
+    fail "Dock at capacity" if dock_full?
+    @bike << bike
+  end
+
+  private
+
+  def dock_full?
+    @bike.length >= @capacity
+  end
+
+  def dock_empty?
+    @bike.empty?
+  end
 end
